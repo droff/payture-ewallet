9d3844f5f4dc3d1daf4ca0488af87643c9526062
Roman Khrebtov <roman@alltmb.ru>
2017-10-20 20:13:05 +0300

    Add pay_url

---
+++ test/payture/methods/pay_url_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+describe Payture::Ewallet::MakePayUrl do
+  before do
+    @client = Payture::Ewallet.client(
+      merchant_id: 'TestMerchant',
+      password: 'merchant12345',
+    )
+  end
+
+  it 'returns payment url' do
+    url = @client.pay_url(session_id: '12345')
+    expected_url = 'https://sandbox.payture.com/vwapi/Pay?SessionId=12345'
+
+    assert_equal expected_url, url
+  end
+end
